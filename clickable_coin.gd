0d0090ff818daa531562f3094d2b6c0cf170aa9e
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2023-12-22 14:54:24 +0000

    spawn small cookie on mouse pos when coin is clicked

--- clickable_coin.gd
+++ clickable_coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal clicked
+@export var small_cookie_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,13 +16,20 @@ func _process(delta):
 func on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
 	if event is InputEventMouseButton and event.pressed == true:
 		clicked.emit()
+		spawn_small_cookie(event.position)
 		show_clicked_animation()
 		
 func show_clicked_animation():
+	if $CoinSprite.is_playing():
+		$CoinSprite.stop()
 	$CoinSprite.play('clicked')
 	
-func spawn_small_cookie():
+func spawn_small_cookie(pos: Vector2):
+	var small_cookie = small_cookie_scene.instantiate()
+	small_cookie.position = pos
 	
+	var main_scene = get_tree().root.get_node("Main")
+	main_scene.add_child(small_cookie)
 	
 func on_coin_sprite_animation_finished():
 	if $CoinSprite.animation == 'clicked':
