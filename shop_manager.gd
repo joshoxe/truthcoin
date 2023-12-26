b92a44fa112dfb8fb1b012e00939dc95d462fa44
Josh <josh@switchsystems.co.uk>
2023-12-26 18:49:34 +0000

    Add a second autominer

--- shop_manager.gd
+++ shop_manager.gd
@@ -11,3 +11,12 @@ func _ready():
 	miner.earn_rate = 0.1
 	miner.image_path = "res://assets/sprites/grandpa.png"
 	miners.append(miner)
+	
+	var miner2 = Miner.new()
+	miner2.miner_name = "The Whiz Kid"
+	miner2.base_cost = 100
+	miner2.cost_multiplier = 1.5
+	miner2.description = "A bright, curious youngster armed with a laptop and a knack for algorithms."
+	miner2.earn_rate = 0.5
+	miner2.image_path = "res://assets/sprites/kid.png"
+	miners.append(miner2)
